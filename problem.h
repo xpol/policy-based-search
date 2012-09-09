b051232cd19f004860ee684a0f3fface1a4e2ca6
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2012-09-09 16:02:45 +1000

    Finished writing most of the search algorithm. Bit the bullet and added all the templates to search(). Changed open list from <Node> to <Node *>. Changed closed from set<Node> to set<State>.

--- problem.h
+++ problem.h
@@ -5,34 +5,30 @@
 
 #include <functional>
 
-namespace search
+namespace jsearch
 {
-	template <typename StepCost, class Node>
-	class EuclideanHeuristic
-	{
-		StepCost h(Node const &A, Node const &B)
-		{
-
-		}
-	};
-
+	// Comparator class, passed to the priority_queue.
 	template <typename StepCost, typename Node, class Heuristic>
-	class GreedyComparator : public std::binary_function<Node, Node, bool>, private Heuristic
+	class AStarComparator : public std::binary_function<Node, Node, bool>, private Heuristic
 	{
 		using Heuristic::h;
 
-		bool operator()(Node const &A, Node const &B)
+		bool operator()(Node const &A, Node const &B) const
 		{
-			return A.path_cost < B.path_cost;
+			return A.path_cost + h(A.state) < B.path_cost + h(B.state);
 		}
 	};
+
+
 
 
 	template <typename State, typename Action, typename PathCost>
 	struct DefaultNode
 	{
+		DefaultNode(State const &STATE, DefaultNode<State, Action, PathCost> const *PARENT, Action const &ACTION, PathCost const &PATH_COST) : state(STATE), parent(PARENT),  action(ACTION), path_cost(PATH_COST) {}
+
 		State state;
-		DefaultNode<State, Action, PathCost> parent;
+		DefaultNode<State, Action, PathCost> const *parent;
 		Action action;
 		PathCost path_cost;
 	};
@@ -50,38 +46,38 @@ namespace search
 
 
 	template < typename ProblemTraits,
-	template <typename State, typename Action> class StepCostPolicy,
-	template <typename State, typename Action> class ActionsPolicy,
-	template <typename State, typename Action> class ResultPolicy,
-	template <typename State> class GoalTestPolicy,
-	template <typename Node, typename State, typename Action,
-	template <typename State, typename Action> class StepCostPolicy,
-	template <typename State, typename Action> class ResultPolicy >
-	class ChildPolicy = DefaultChildPolicy >
+			 template <typename State, typename Action> class StepCostPolicy,
+			 template <typename State, typename Action> class ActionsPolicy,
+			 template <typename State, typename Action> class ResultPolicy,
+			 template <typename State> class GoalTestPolicy,
+			 template < typename Node, typename State, typename Action,
+			 template <typename State, typename Action> class StepCostPolicy,
+			 template <typename State, typename Action> class ResultPolicy >
+			 class ChildPolicy = DefaultChildPolicy >
 	class Problem :
-	private StepCostPolicy<typename ProblemTraits::state, typename ProblemTraits::action>,
+		private StepCostPolicy<typename ProblemTraits::state, typename ProblemTraits::action>,
 		private ActionsPolicy<typename ProblemTraits::state, typename ProblemTraits::action>,
-			private ResultPolicy<typename ProblemTraits::state, typename ProblemTraits::action>,
-				private GoalTestPolicy<typename ProblemTraits::state>,
-					private ChildPolicy<typename ProblemTraits::node, typename ProblemTraits::state, typename ProblemTraits::action, StepCostPolicy, ResultPolicy>
-					{
-					public:
-						Problem(typename ProblemTraits::state const &INITIAL) : INITIAL(INITIAL) {}
+		private ResultPolicy<typename ProblemTraits::state, typename ProblemTraits::action>,
+		private GoalTestPolicy<typename ProblemTraits::state>,
+		private ChildPolicy<typename ProblemTraits::node, typename ProblemTraits::state, typename ProblemTraits::action, StepCostPolicy, ResultPolicy>
+	{
+	public:
+		Problem(typename ProblemTraits::state const &INITIAL) : INITIAL(INITIAL) {}
 
-						typename ProblemTraits::state initial()
-						{
-							return INITIAL;
-						}
+		typename ProblemTraits::state initial() const
+		{
+			return INITIAL;
+		}
 
-						using ChildPolicy<typename ProblemTraits::node, typename ProblemTraits::state, typename ProblemTraits::action, StepCostPolicy, ResultPolicy>::child;
-						using StepCostPolicy<typename ProblemTraits::state, typename ProblemTraits::action>::step_cost;
-						using ActionsPolicy<typename ProblemTraits::state, typename ProblemTraits::action>::actions;
-						using ResultPolicy<typename ProblemTraits::state, typename ProblemTraits::action>::result;
-						using GoalTestPolicy<typename ProblemTraits::state>::goal_test;
+		using ChildPolicy<typename ProblemTraits::node, typename ProblemTraits::state, typename ProblemTraits::action, StepCostPolicy, ResultPolicy>::child;
+		using StepCostPolicy<typename ProblemTraits::state, typename ProblemTraits::action>::step_cost;
+		using ActionsPolicy<typename ProblemTraits::state, typename ProblemTraits::action>::actions;
+		using ResultPolicy<typename ProblemTraits::state, typename ProblemTraits::action>::result;
+		using GoalTestPolicy<typename ProblemTraits::state>::goal_test;
 
-					private:
-						typename ProblemTraits::state const INITIAL;
-					};
+	private:
+		typename ProblemTraits::state const INITIAL;
+	};
 }
 
 #endif
--- search.h
+++ search.h
@@ -1,26 +1,77 @@
 
+#ifndef SEARCH_H
+#define SEARCH_H
+
 #include "problem.h"
 
 #include <queue>
 #include <set>
+#include <algorithm>
+#include <iostream>
 
-namespace search
+namespace jsearch
 {
-	template <typename Traits>
-	typename Traits::node bestfirstsearch(Problem const &PROBLEM)
+	template <typename Traits,
+			template <typename State, typename Action> class StepCostPolicy,
+			template <typename State, typename Action> class ActionsPolicy,
+			template <typename State, typename Action> class ResultPolicy,
+			template <typename State> class GoalTestPolicy,
+			template <typename Node, typename State, typename Action,
+				template <typename State, typename Action> class StepCostPolicy,
+				template <typename State, typename Action> class ResultPolicy >
+					class ChildPolicy = DefaultChildPolicy>
+	typename Traits::node *search(Problem<Traits, StepCostPolicy, ActionsPolicy, ResultPolicy, GoalTestPolicy, ChildPolicy> const &PROBLEM, bool const combinatorial)
 	{
 		typedef typename Traits::node Node;
-		Node solution;
+		typedef typename Traits::action Action;
+		typedef typename Traits::state State;
 
-		std::priority_queue<Node> open;
-		std::set<Node> closed;
-		open.add(typename Traits::node(null, null, 0, INITIAL));
+		Node *solution = nullptr;
+
+		std::priority_queue<Node *> open;
+		std::set<State> closed;
+		open.push(new Node(PROBLEM.initial(), (nullptr), 0, 0));
 
 		while(!solution && !open.empty())
 		{
+			Node * const S = open.top();
+			open.pop();
+
+			std::cerr << "open: " << open.size() << ", closed: " << closed.size() << "\n";
+
+			if(PROBLEM.goal_test(S->state))
+			{
+				solution = S;
+			}
+			else
+			{
+				closed.insert(S->state);
+				std::set<Action> const actions(PROBLEM.actions(S->state));
+				typename std::set<Action>::const_iterator beginning = actions.begin();
+				typename std::set<Action>::const_iterator ending = actions.end();
+				std::for_each(beginning, ending, [&](typename std::set<Action>::const_reference ACTION)
+				{
+					Node *child = new Node(PROBLEM.result(S->state, ACTION), S, ACTION, S->path_cost + PROBLEM.step_cost(ACTION));
+
+					if(!combinatorial)
+					{
+						// TODO: Check if it is in open or closed.
+
+					}
+					else
+					{
+						/*	The combinatorial search spaces in mind are a tree with no repeating nodes,
+							so the algorithm is optimized to not worry about checking in open or closed.	*/
+						open.push(child);
+					}
+
+				});
+			}
 
 		}
 
 		return solution;
 	}
 }
+
+#endif // SEARCH_H
